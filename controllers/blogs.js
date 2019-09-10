7ada6a2a7314f732963dd5038969259c3fe3bf7e
Janssen Kuhn <noconsulate@gmail.com>
2019-09-09 20:28:50 -0600

    good

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -21,11 +21,36 @@ blogsRouter.post('/', async (request, response, next) => {
       likes: likes
     })
 
-
   try {
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog.toJSON())
   } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const blog = await Blog.findById(request.params.id)
+  const updated = {
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes + 1
+  }
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, updated, 
+      {new: true})
+    response.json(updatedBlog.toJSON())
+  } catch {
     next(exception)
   }
 })
